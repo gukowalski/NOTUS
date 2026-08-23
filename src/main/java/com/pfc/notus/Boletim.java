815c586ae6a80d0eaf65e881a587dcf32c2e037f
Lucas <margenetlucas@gmail.com>
2026-08-23 19:27:47 -0300

    Adicionando classe Boletim e validando criação

---
+++ src/main/java/com/pfc/notus/Boletim.java
@@ -0,0 +1,49 @@
+package com.pfc.notus;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name ="tb_boletim")
+@NoArgsConstructor
+public class Boletim {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Getter
+    private Long id;
+    @Getter @Setter
+    private String period;
+    @Getter @Setter
+    private Float finalAverage;
+    @Getter @Setter
+    private String status;
+
+    public Boletim(String period, Float finalAverage){
+        this.period = period;
+        this.finalAverage = finalAverage;
+        this.status = "Status";
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Float getFinalAverage() {
+        return finalAverage;
+    }
+
+    public void setFinalAverage(Float finalAverage) {
+        this.finalAverage = finalAverage;
+    }
+}
