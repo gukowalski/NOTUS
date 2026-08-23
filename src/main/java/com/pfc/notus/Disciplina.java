b685f9cd1c7b009b2a4469afbe2f3a2b288d7ed2
Lucas <margenetlucas@gmail.com>
2026-08-23 19:46:35 -0300

    Adicionando classe Disciplina e validando criação

---
+++ src/main/java/com/pfc/notus/Disciplina.java
@@ -0,0 +1,32 @@
+package com.pfc.notus;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name ="tb_disciplina")
+@NoArgsConstructor
+public class Disciplina {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    private Long id;
+    @Getter @Setter
+    private String title;
+    @Getter @Setter
+    private String description;
+    @Getter @Setter
+    private LocalDateTime created;
+
+    public Disciplina(String title, String description) {
+        this.title = title;
+        this.description = description;
+        this.created = LocalDateTime.now();
+    }
+}
