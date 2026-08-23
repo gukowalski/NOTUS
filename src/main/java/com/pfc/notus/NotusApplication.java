c53763ca1a42e3cbc6a01bffc0d128f1dab9f667
Gustavo Kowalski <kkowalskigustavo@gmail.com>
2026-08-22 23:43:25 -0300

    inital commit notus

---
+++ src/main/java/com/pfc/notus/NotusApplication.java
@@ -0,0 +1,13 @@
+package com.pfc.notus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pfc/notus/NotusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pfc.notus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
