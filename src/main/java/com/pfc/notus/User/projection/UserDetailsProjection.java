cbaca2752bd6ca376f92bd8e5de6fe6c03c4e2e5
Gustavo Kowalski <kkowalskigustavo@gmail.com>
2026-08-26 20:06:33 -0300

    add User and role repository with Query to search User and Role by email

---
+++ src/main/java/com/pfc/notus/User/projection/UserDetailsProjection.java
@@ -0,0 +1,8 @@
+package com.pfc.notus.User.projection;
+
+public interface UserDetailsProjection {
+    String getUsername();
+    String getPassword();
+    Long getRoleId();
+    String getAuthority();
+}
