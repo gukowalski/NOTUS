c3415daae3c385839352aa91f95ad56c94e576c3
Gustavo Kowalski <kkowalskigustavo@gmail.com>
2026-08-26 20:02:41 -0300

    add User and Roles entity

---
+++ src/main/java/com/pfc/notus/User/domain/Role.java
@@ -0,0 +1,28 @@
+package com.pfc.notus.User.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Table(name = "tb_role")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role implements GrantedAuthority  {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    private Long id;
+
+    @Getter @Setter
+    private String authority;
+
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
+}
---
+++ src/main/java/com/pfc/notus/User/domain/User.java
@@ -0,0 +1,82 @@
+package com.pfc.notus.User.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_user")
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter
+    private Long id;
+
+    @Getter @Setter
+    private String username;
+    @Getter @Setter
+    private String password;
+
+    @Getter @Setter
+    private String email;
+
+    @Getter @Setter
+    private String phone;
+
+    @Getter @Setter
+    private String address;
+
+    @ManyToMany
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
