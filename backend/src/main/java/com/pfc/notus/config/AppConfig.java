cbdb682b82bf38df80663546096ae07f9269734c
Gustavo Kowalski <kkowalskigustavo@gmail.com>
2026-09-02 18:41:10 -0300

    new branch

--- backend/src/main/java/com/pfc/notus/config/AppConfig.java
+++ backend/src/main/java/com/pfc/notus/config/AppConfig.java
@@ -21,7 +21,7 @@ public class AppConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
+//
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
